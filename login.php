18da6894320d2263289ee0cd9d0e81d1c13b0342
zenyhoj <joe.balingit@gmail.com>
2018-02-19 11:53:24 +0800

    configured mysql connection and created insert query: OK

--- login.php
+++ login.php
@@ -15,29 +15,33 @@
 <!--LOGIN FORM-->
     <div class="container">
         <div class="row">
-            <div class="col-lg-8">
-                <form>
+            <div class="col-lg-4 login-form-info">
+                <h3>Subscribe to our Newsletter!</h3>
+            </div>
+            <div class="col-lg-6 login-form">
+                <form action="login_process.php" method="post">
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Username" required>
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
                     </div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+<!--                    <div class="form-check">-->
+<!--                        <input type="checkbox" class="form-check-input" id="exampleCheck1">-->
+<!--                        <label class="form-check-label" for="exampleCheck1">Check me out</label>-->
+<!--                    </div>-->
+                    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
 
                 </form>
 
             </div>
 
+
             </div>
-            <div class="col-lg-4"></div>
+
         </div>
 
 
--- login_process.php
+++ login_process.php
@@ -1,2 +1,26 @@
 <?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+//mysql connection
+$conn = mysqli_connect('localhost', 'root', '', 'login_credentials');
+//Creating a new user
+$query = "INSERT INTO user_accounts (username, password) VALUES ('$username', '$password')";
+$result = mysqli_query($conn, $query);
+if(!$result) {
+	die('Query failed!' . mysqli_error($conn));
+} else {
+	echo 'successfully added new user';
+}
+//mysql connection ends here
+//echo "<br>";
+//
+//if ( !empty($username) && !empty($password) ){
+//	if($username === $password){
+//		echo "hi $username";
+//	}
+//
+//} else {
+//	echo "Your username and password did not match, please input it correctly";
+//}
+
 
