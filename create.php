bd1a0005da2440d2d7efe84096c1102d030c3b71
= <=>
2018-02-28 14:59:33 +0800

    refactored code, coded delete_users.php

--- create.php
+++ create.php
@@ -1,10 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create User Form</title>
+</head>
+<body>
+
+
 <?php
-/**
- * Created by PhpStorm.
- * User: joe
- * Date: 2/28/18
- * Time: 10:48 AM
- */
 $username = $_POST['username'];
 $password = $_POST['password'];
 //Creating a new user
@@ -16,3 +22,15 @@ if(!$result) {
 } else {
     echo "<h3>{$username} added successfully</h3>";
 }
+?>
+
+<form action="create.php" method="post">
+
+    <input type="text" name="username" placeholder="Enter your username here" />
+    <input type="password" name="password" placeholder="Your password here" />
+    <input type="submit" name="submit" value="Submit">
+
+</form>
+
+</body>
+</html>
--- delete_users.php
+++ delete_users.php
@@ -1,5 +1,5 @@
 <?php include "dbcon.php";
-//use in options field for id
+
 $query = "SELECT * FROM users";
 $result = mysqli_query($conn, $query);
 if(!$result){
@@ -14,12 +14,12 @@ if(isset($_POST['submit'])){
     $password = $_POST['password'];
     $id = $_POST['id'];
 
-    $query_update = "UPDATE users SET username = '$username', password = '$password' WHERE id=$id";
-    $result = mysqli_query($conn, $query_update);
+    $query_delete = "DELETE FROM users WHERE id=$id";
+    $result = mysqli_query($conn, $query_delete);
     if(!$result){
-        die("Error updating users" . mysqli_error($conn));
+        die("Error deleting users" . mysqli_error($conn));
     } else{
-        echo 'successfully updating users';
+        echo "successfully deleted user with id {$id}";
     }
 
 }
@@ -33,14 +33,14 @@ if(isset($_POST['submit'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User Update Form</title>
+    <title>User Delete Form</title>
 </head>
 <body>
 
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <form action="update_users.php" method="post">
+            <form action="delete_users.php" method="post">
 
                 <input type="text" name="username" placeholder="Enter your username here" />
                 <input type="password" name="password" placeholder="Your password here" />
