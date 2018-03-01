12b2bc88c76dd271f0a62bb3b983d706677bf14a
= <=>
2018-03-01 09:44:49 +0800

    added bootstrap 4 assets

---
+++ read.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Create User Form</title>
+</head>
+<body>
+
+
+<?php
+//Creating a new user
+include "dbcon.php";
+$query = "SELECT * FROM users";
+$result = mysqli_query($conn, $query);
+if(!$result) {
+    die('Query failed!' . mysqli_error($conn));
+}
+?>
+
+<table class="table table-dark">
+    <thead>
+    <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Username</th>
+        <th scope="col">Password</th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php
+        while($row=mysqli_fetch_assoc($result)) {
+            $id = $row['id'];
+            $username = $row['username'];
+            $password = $row['password'];
+            echo "<tr>";
+       echo "<th scope='row''>$id</th>";
+        echo "<td>$username</td>";
+        echo "<td>$password</td>";
+        echo "</tr>";
+   }
+        ?>
+    </tbody>
+</table>
+<script src="assets/js/jquery.slim.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
