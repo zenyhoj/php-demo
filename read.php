094b4c4e2e3d578347ed17d4e0984ed9e7264b3d
= <=>
2018-03-01 09:55:00 +0800

    coded read.php and some styles.css edits

--- read.php
+++ read.php
@@ -5,8 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <title>Create User Form</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Users</title>
 </head>
 <body>
 
@@ -14,36 +15,42 @@
 <?php
 //Creating a new user
 include "dbcon.php";
-$query = "SELECT * FROM users";
+$query = "SELECT * FROM users ORDER BY id DESC";
 $result = mysqli_query($conn, $query);
 if(!$result) {
     die('Query failed!' . mysqli_error($conn));
 }
 ?>
+<div class="container">
 
-<table class="table table-dark">
-    <thead>
-    <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Username</th>
-        <th scope="col">Password</th>
-    </tr>
-    </thead>
-    <tbody>
-        <?php
-        while($row=mysqli_fetch_assoc($result)) {
-            $id = $row['id'];
-            $username = $row['username'];
-            $password = $row['password'];
-            echo "<tr>";
-       echo "<th scope='row''>$id</th>";
-        echo "<td>$username</td>";
-        echo "<td>$password</td>";
-        echo "</tr>";
-   }
-        ?>
-    </tbody>
-</table>
+    <div class="row">
+        <div class="col-md-6 col-centered">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col" class="text-center">ID</th>
+                    <th scope="col" class="text-center">Username</th>
+                    <th scope="col" class="text-center">Password</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                while($row=mysqli_fetch_assoc($result)) {
+                    $id = $row['id'];
+                    $username = $row['username'];
+                    $password = $row['password'];
+                    echo "<tr>";
+                    echo "<th scope='row' class='text-center'>$id</th>";
+                    echo "<td class='text-center'>$username</td>";
+                    echo "<td class='text-center'>$password</td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 <script src="assets/js/jquery.slim.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
 </body>
