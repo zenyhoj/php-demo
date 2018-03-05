93396dd51db09408295d1a45085cace433e37d90
= <=>
2018-03-05 15:53:21 +0800

    refactored create user function

--- functions.php
+++ functions.php
@@ -9,13 +9,15 @@ global $conn;
     $username = mysqli_real_escape_string($conn, $_POST['username']); //prevent mysql injection
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 //Creating a new user
-    $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+if(!empty($username) && !empty($password)){
+$query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
     $result = mysqli_query($conn, $query);
-    if(!$result) {
+
+ echo "<h3>{$username} added successfully</h3>";
+}else{
         die('Query failed!' . mysqli_error($conn));
-    } else {
-        echo "<h3>{$username} added successfully</h3>";
     }
+    
 
 }
 createUser();
